a2af91728865720e48a59f4137b9e7a8b8011884
fredafei <964959033@qq.com>
2020-06-21 18:40:20 +0800

    优化代码

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::API
   # include ActionView::Layouts
+
+  def render_resource(resource)
+    if resource.errors.empty?
+      render json: {resource: resource}, status: 200
+    else
+      render json: {errors: resource.errors}, status: 400
+    end
+  end
 end
