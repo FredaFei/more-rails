7e1a104f2d3311e23dd3a1e2379658dd4b8ded03
fredafei <964959033@qq.com>
2020-07-09 22:56:42 +0800

    更新 records api docs 测试

--- spec/acceptance/records_spec.rb
+++ spec/acceptance/records_spec.rb
@@ -4,7 +4,7 @@ require 'rspec_api_documentation/dsl'
 resource "records" do
   post "/records" do
     parameter :amount, '金额', type: :integer, required: true
-    parameter :category, '类型: outgoings|income', type: :string, required: true
+    parameter :category, '类型: 1 outgoings|2 income', type: :string, required: true
     parameter :notes, '备注', type: :string
     example "创建记录" do
       do_request(amount: 10000, category: 'outgoings', notes: '吃饭')
