6f6e052bd4753cafe948b4516b882eacc2c0dce2
fredafei <964959033@qq.com>
2020-07-09 22:39:22 +0800

    添加 records api docs 测试

---
+++ spec/acceptance/records_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+require 'rspec_api_documentation/dsl'
+
+resource "records" do
+  post "/records" do
+    parameter :amount, '金额', type: :integer, required: true
+    parameter :category, '类型: outgoings|income', type: :string, required: true
+    parameter :notes, '备注', type: :string
+    example "创建记录" do
+      do_request(amount: 10000, category: 'outgoings', notes: '吃饭')
+      expect(status).to eq 200
+    end
+  end
+end
