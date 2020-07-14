c2417340e2ac7a4fd6bb96601c83f64c18268101
fredafei <964959033@qq.com>
2020-07-14 22:44:46 +0800

    添加 tagging controller

---
+++ app/controllers/taggings_controller.rb
@@ -0,0 +1,28 @@
+class TaggingsController < ApplicationController
+
+  before_action :must_sign_in
+
+  def index
+    render_resources Tagging.page(params[:page])
+  end
+
+  def create
+    render_resource Tagging.create create_params
+  end
+
+  def show
+    render_resource Tagging.find params[:id]
+  end
+
+  def destroy
+    tagging = Tagging.find params[:id]
+    head tagging.destroy ? :ok : :bad_request
+  end
+
+  private
+
+  def create_params
+    params.permit(:record_id, :tag_id)
+  end
+
+end
