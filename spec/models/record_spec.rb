f3308dc1802e6fcc7ab135a930284064de84adf3
fredafei <964959033@qq.com>
2020-10-25 11:43:12 +0800

    更新 record 测试

--- spec/models/record_spec.rb
+++ spec/models/record_spec.rb
@@ -1,12 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Record, type: :model do
-  it '创建时必有amount' do
+  it '创建时必有email' do
     record = Record.create category: 'outgoings', notes: '吃饭'
     expect(record.errors.details[:amount][0][:error]).to eq :blank
     expect(record.errors.details[:amount][0].length).to eq 1
   end
-  it '创建时必有category' do
+  it '创建时必有password' do
     record = Record.create amount: 100000, notes: '吃饭'
     expect(record.errors.details[:category][0][:error]).to eq :blank
     expect(record.errors.details[:category][0].length).to eq 1
